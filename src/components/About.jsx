43b7baf15a5c7e66516a282586cccdc5b0141002
panickus <felipedg@outlook.es>
2024-03-15 20:57:16 +0100

    Complete Feedback component and fixed Hero component.

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -10,7 +10,7 @@ import { fadeIn, textVariant } from '../utils/motion';
 const ServiceCard = ({ index, title, icon }) => (
     <Tilt className="xs:w-[250px] w-full">
       <motion.div
-        variants={fadeIn("right", "spring", 0.5 * index, 0.75)}
+        variants={fadeIn("right", "spring", index * 0.5, 0.75)}
         className='w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card'
       >
         <div
@@ -21,6 +21,11 @@ const ServiceCard = ({ index, title, icon }) => (
           }}
           className='bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col'
         >
+          <img
+          src={icon}
+          alt='web-development'
+          className='w-16 h-16 object-contain'
+        />
           <h3 className='text-white text-[20px] font-bold text-center'>{title}</h3>
         </div>
       </motion.div>
